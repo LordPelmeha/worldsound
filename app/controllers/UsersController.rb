bb25395a6f9d4b7bde2ea7e6bb9e9160024525a7
LordPelmeha <sofolov@sfedu.ru>
2024-12-26 02:11:46 +0300

    I still hate firebase

--- app/controllers/UsersController.rb
+++ app/controllers/UsersController.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'firebase'
 
 class UsersController < ApplicationController
   def signup
@@ -44,6 +45,33 @@ class UsersController < ApplicationController
         flash[:error] = translate_error(data['error']['message'])
         render :login
       end
+    end
+  end
+
+  def dashboard
+    # Получение ID текущего пользователя
+    user_id = session[:user_id]
+
+    # Получаем данные пользователя через Firebase Authentication
+    firebase_auth_client = FirebaseAuth.client
+    user_data = firebase_auth_client.get("users/#{user_id}")
+
+    if user_data.success?
+      @user = user_data.body
+    else
+      flash[:error] = "Не удалось загрузить данные пользователя."
+      redirect_to root_path
+      return
+    end
+
+    # Получаем сообщения пользователя через Realtime Database
+    firebase_db_client = FirebaseDB.client
+    messages_data = firebase_db_client.get("messages/#{user_id}")
+
+    if messages_data.success?
+      @messages = messages_data.body
+    else
+      flash[:error] = "Не удалось загрузить сообщения."
     end
   end
 
--- config/initializers/fiebase.rb
+++ config/initializers/fiebase.rb
@@ -1,5 +1,18 @@
 require 'firebase'
 
-base_uri = ENV['FIREBASE_BASE_URI']
-private_key_json = File.read(ENV['FIREBASE_CREDENTIALS'])
-firebase = Firebase::Client.new(base_uri, private_key_json)
+#base_uri = ENV['FIREBASE_BASE_URI']
+#auth_uri = ENV['FIREBASE_AUTH_URI']
+#private_key_json = File.read(ENV['FIREBASE_CREDENTIALS'])
+#firebase = Firebase::Client.new(base_uri, private_key_json)
+class FirebaseAuth
+  def self.client
+    Firebase::Client.new(ENV['FIREBASE_AUTH_URI'], File.read(ENV['FIREBASE_CREDENTIALS']))
+  end 
+end
+
+# Класс для работы с Realtime Database
+class FirebaseDB
+  def self.client
+    Firebase::Client.new(ENV['FIREBASE_BASE_URI'], File.read(ENV['FIREBASE_CREDENTIALS']))
+  end
+end
